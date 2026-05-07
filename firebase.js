dbf6e1789243b9f0ec96f1de5a291ebd4e69741b
AnTI9987 <antivirusgeometrydash@gmail.com>
2026-05-08 02:31:38 +0300

    Refactor Firebase imports and exports

--- firebase.js
+++ firebase.js
@@ -1,25 +1,34 @@
-/* ── js/firebase.js ── */
-import { initializeApp }
-  from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-import { getDatabase, ref, set, get }
-  from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged }
-  from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+// js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+import { getDatabase, ref, set, get, onValue } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 
+// ==================== FIREBASE CONFIG ====================
 const firebaseConfig = {
-  apiKey:            "AIzaSyAcv5AfJPjUA-RGfcAsUiQwvucSxkJX4F0",
-  authDomain:        "anti-shop-99f1d.firebaseapp.com",
-  databaseURL:       "https://anti-shop-99f1d-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId:         "anti-shop-99f1d",
-  storageBucket:     "anti-shop-99f1d.firebasestorage.app",
-  messagingSenderId: "347202416802",
-  appId:             "1:347202416802:web:2d2df1b819be9da180e7fb"
+    apiKey: "AIzaSyAcv5AfJPjUA-RGfcAsUiQwvucSxkJX4F0",
+    authDomain: "anti-shop-99f1d.firebaseapp.com",
+    databaseURL: "https://anti-shop-99f1d-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "anti-shop-99f1d",
+    storageBucket: "anti-shop-99f1d.firebasestorage.app",
+    messagingSenderId: "347202416802",
+    appId: "1:347202416802:web:2d2df1b819be9da180e7fb"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
-export const db       = getDatabase(app);
-export const auth     = getAuth(app);
-export const provider = new GoogleAuthProvider();
-
-export { ref, set, get, signInWithPopup, signOut, onAuthStateChanged };
+export {
+    db,
+    auth,
+    provider,
+    ref,
+    set,
+    get,
+    onValue,
+    onAuthStateChanged,
+    signInWithPopup,
+    signOut
+};
